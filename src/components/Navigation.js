258b1f7b4b69fc59b0fb71a5fed412d22c053431
Choi <sparrow42@naver.com>
2020-12-14 05:29:16 +0900

    done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import "./Navigation.css";
 
 function Navigation(){
-    return (<div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/detail">Detail</Link>
+    return (<div className= 'nav-container'>
+        <Link to="/" className='nav-item'>Home</Link>
+        <Link to="/about" className='nav-item'>About</Link>
+        {/* <Link to="/detail">Detail</Link> */}
     </div>
     );
 }
